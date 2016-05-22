8d0ee67672b1cba81a406b8b83a40106c2ee82fa
Andrzej Swaton <andrzej.swaton@gmail.com>
2016-05-22 16:31:53 +0200

    rpi-gpio

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var gpio = require('rpi-gpio');
+ 
+// gpio.setup(7, gpio.DIR_OUT, write);
+// gpio.setup(8, gpio.DIR_OUT, write);
+// gpio.setup(9, gpio.DIR_OUT, write);
+ 
+function writeToPin(pinNumber) {
+    gpio.write(pinNumber, true, function(err) {
+        if (err) throw err;
+        console.log('Written to pin' + pinNumber);
+    });
+}
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+
+router.get('/write7', function(req, res, next) {
+   writeToPin(7);
+   res.json({'written' : 7})
+  //res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
