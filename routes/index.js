78b9b8fc51f54c39c7da8bec81ee6e6cfbc723c5
Andrzej Swaton <andrzej.swaton@gmail.com>
2016-05-22 17:19:15 +0200

    writing to pin 7 should work

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,43 @@
 var express = require('express');
 var router = express.Router();
-var gpio = require('rpi-gpio');
+var gpio;
+try {
+ gpio = require('rpi-gpio'); 
+}
+catch(err) {
+ console.warn('gpio require failed, creating stub for os not supporting rpi-gpio');
+ gpio = {
+  write(pinNumber, value, cb) {
+    console.log(`would normally write ${value} to ${pinNumber}`);
+    setTimeout(cb, 1000);
+  },
+  setup(pinNumber) {
+    console.log(`would normally setup on ${pinNumber}`);
+  }
+ }
+}
+
+function GpioOut(pinNumber) {
+  gpio.setup(pinNumber, gpio.DIR_OUT, () => console.log('set up on ' + pinNumber));
+  return {
+    getPinNumber: () => pinNumber,
+    on: (cb) => gpio.write(pinNumber, true, cb) 
+  }
+}
+
  
 // gpio.setup(7, gpio.DIR_OUT, write);
 // gpio.setup(8, gpio.DIR_OUT, write);
 // gpio.setup(9, gpio.DIR_OUT, write);
  
-function writeToPin(pinNumber) {
-    gpio.write(pinNumber, true, function(err) {
-        if (err) throw err;
-        console.log('Written to pin' + pinNumber);
-    });
-}
+ GpioOut7 = GpioOut(7);
 
 
 
 router.get('/write7', function(req, res, next) {
-   writeToPin(7);
-   res.json({'written' : 7})
+  console.log('router get')
+   GpioOut7.on(() => res.json({'written' : GpioOut7.getPinNumber()}));
+   
   //res.render('index', { title: 'Express' });
 });
 
