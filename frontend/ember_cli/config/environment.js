99702e060b8dbd46dd55620bc62672ceb38bcbcc
jedrula <andrzej.swaton@gmail.com>
2017-04-23 11:30:51 +0200

    update my shaky ip for productin

--- frontend/ember_cli/config/environment.js
+++ frontend/ember_cli/config/environment.js
@@ -52,18 +52,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    // steps i took to setup a https
-    // 1. created a free domain at freenom
-    // 2. registered the domain in cloudfare
-    // 3. changed the nameservers in freenom from freenom's to cloudfare's
-    // 4. waited and waited... lets see if it worked
-    // ... maybe https will need to be added to nodejs?
-
-    // I am trying to start with somthing like client -> cloudfare server -> raspberry
-    // ENV.APP.API_HOST = 'https://78.88.255.144:3001';
-    // https://www.cloudflare.com/a/crypto/raspberrycar.tk
-    // https://my.freenom.com/clientarea.php?managedns=raspberrycar.tk&domainid=1025525855
-    ENV.APP.API_HOST = 'https://raspberrycar.tk:3001';
+    ENV.APP.API_HOST = 'http://78.88.254.231:3001';
   }
 
   return ENV;
