818110f6616909df45422ec80e18b69b84f3606e
jedrula <andrzej.swaton@gmail.com>
2017-04-23 11:27:23 +0200

    expose camera feed to production - hardcoded

--- frontend/ember_cli/app/components/live-feed/component.js
+++ frontend/ember_cli/app/components/live-feed/component.js
@@ -4,11 +4,9 @@ export default Ember.Component.extend({
   didRender() {
       setTimeout(() => {
         const canvas = this.$('canvas').get(0);
-        var uri = "ws://192.168.0.155:8080";
+        var uri = "ws://78.88.254.231:5000";
         var wsavc = new WSAvcPlayer(canvas, "webgl", 1, 35);
         wsavc.connect(uri);
-        //expose instance for button callbacks
-        window.wsavc = wsavc;
         setTimeout(function() {
           wsavc.playStream()
         }, 2000)
