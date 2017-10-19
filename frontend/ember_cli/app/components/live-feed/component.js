12ec914c7677430d936bcab5d8cac8ba24f80778
jedrula <andrzej.swaton@gmail.com>
2017-10-19 18:07:32 +0200

    improvements

--- frontend/ember_cli/app/components/live-feed/component.js
+++ frontend/ember_cli/app/components/live-feed/component.js
@@ -2,14 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   didRender() {
-      setTimeout(() => {
-        const canvas = this.$('canvas').get(0);
-        var uri = "ws://78.88.254.231:5000";
-        var wsavc = new WSAvcPlayer(canvas, "webgl", 1, 35);
-        wsavc.connect(uri);
-        setTimeout(function() {
-          wsavc.playStream()
-        }, 2000)
-    }, 2000)
+    // setTimeout(() => {
+      const canvas = this.$('canvas').get(0);
+      var uri = "ws://192.168.1.102:80/video-stream";
+      var wsavc = new WSAvcPlayer(canvas, "webgl"); // , 1, 35
+      wsavc.connect(uri);
+      // setTimeout(function() {
+      //   wsavc.playStream()
+      // }, 2000)
+    // }, 2000)
   }
 });
