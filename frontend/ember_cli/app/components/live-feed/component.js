5f0c9969b2de6ac72e14a6765866cd4486eb35cf
jedrula <andrzej.swaton@gmail.com>
2017-10-20 00:06:59 +0200

    simplify clientside feed

--- frontend/ember_cli/app/components/live-feed/component.js
+++ frontend/ember_cli/app/components/live-feed/component.js
@@ -4,8 +4,9 @@ export default Ember.Component.extend({
   didRender() {
     // setTimeout(() => {
       const canvas = this.$('canvas').get(0);
-      var uri = "ws://192.168.1.102:80/video-stream";
-      var wsavc = new WSAvcPlayer(canvas, "webgl"); // , 1, 35
+      const uri = "ws://192.168.1.102:80/video-stream";
+      // const uri = "wss://4de93a31a4324b5dfa4e5e7881681c56.resindevice.io/video-stream";
+      const wsavc = new WSAvcPlayer(canvas, "webgl"); // , 1, 35
       wsavc.connect(uri);
       // setTimeout(function() {
       //   wsavc.playStream()
