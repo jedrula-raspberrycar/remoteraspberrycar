1b2cd82c12a17ebaeafe47a5d59442693bc3ec7f
jedrula <andrzej.swaton@gmail.com>
2017-10-21 19:33:14 +0200

    fix pwm

--- scripts/pwm.js
+++ scripts/pwm.js
@@ -6,16 +6,16 @@ const two = PWM(15 || process.env.PIN2);
 const waitOneSec = cb => setTimeout(cb, 1000);
 
 setTimeout(() => {
-  console.log('seting off');
+  console.log('pwm script');
   async.series([
-    cb => { one.off(); two.off(); cb() },
+    cb => { one.off(); two.off(); cb(); },
     waitOneSec,
-    cb => { one.on(); two.off(); cb() }
+    cb => { one.on(); two.off(); cb(); }
     waitOneSec,
-    cb => { one.off(); two.off(); cb() },
+    cb => { one.off(); two.off(); cb(); },
     waitOneSec,
-    cb => { one.off(); two.on(); cb() },
+    cb => { one.off(); two.on(); cb(); },
     waitOneSec,
-    cb => { one.off(); two.off(); cb() },
+    cb => { one.off(); two.off(); cb(); },
   ]);
 }, 1000);
