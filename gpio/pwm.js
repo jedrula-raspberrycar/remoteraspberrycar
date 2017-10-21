0d6278b50e1463b482c56dd94aefa8acc7d53450
jedrula <andrzej.swaton@gmail.com>
2017-10-21 19:43:46 +0200

    fixes and package json

--- gpio/pwm.js
+++ gpio/pwm.js
@@ -2,27 +2,27 @@
 const raspi = require('raspi');
 const pwm = require('raspi-pwm');
 
-raspi.init(() => {
-  const led = new pwm.PWM('P1-12');
-  led.write(0.5); // 50% Duty Cycle, aka half brightness
-});
-
 module.exports = {
   PWM(pinNumber) {
-    const pwm = new pwm.PWM(`P1-${pinNumber}`);
+    // TODO not most robust ! improve when more time
+    let pwmPin;
+    raspi.init(() => {
+      pwmPin = new pwm.PWM(`P1-${pinNumber}`);
+    });
+
     return {
       getPinNumber: () => pinNumber,
       on() {
         console.log('lets gpio.write true on ' + pinNumber);
-        pwm.write(1);
+        pwmPin.write(1);
       },
       off() {
         console.log('lets gpio.write false on ' + pinNumber);
-        pwm.write(0);
+        pwmPin.write(0);
       },
       write(dutyCycle) {
-        pwm.write(dutyCycle);
+        pwmPin.write(dutyCycle);
       }
     }
   }
-}
+};
