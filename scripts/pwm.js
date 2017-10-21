ca2d06394635e52aa91b52a1613bc6d270a15861
jedrula <andrzej.swaton@gmail.com>
2017-10-21 20:03:51 +0200

    speed up

--- scripts/pwm.js
+++ scripts/pwm.js
@@ -3,6 +3,7 @@ const PWM = require('../gpio/pwm').PWM;
 const one = PWM(13 || process.env.PIN1);
 const two = PWM(15 || process.env.PIN2);
 
+/*
 const waitOneSec = cb => setTimeout(cb, 1000);
 
 setTimeout(() => {
@@ -17,5 +18,41 @@ setTimeout(() => {
     cb => { one.off(); two.on(); cb(); },
     waitOneSec,
     cb => { one.off(); two.off(); cb(); },
+  ]);
+}, 1000);
+*/
+
+
+
+two.off();
+one.write(0.1);
+
+function speedUp(first, second, cb) {
+  first.off();
+  second.off();
+  increaseSpeed(0, first, cb)
+}
+
+
+
+function increaseSpeed(speed, pin, cb) {
+  if (speed >= 1) {
+      pin.off();
+      cb();
+    } else {
+      const newSpeed = speed + 0.05;
+      pin.write(newSpeed);
+      setTimeout(() => increaseSpeed(newSpeed, pin), 200);
+    }
+}
+
+
+setTimeout(() => {
+  console.log('pwm script');
+  async.series([
+    cb => { speedUp(one, two, cb) },
+    waitOneSec,
+    cb => { speedUp(two, one, cb) },
+    waitOneSec,
   ]);
 }, 1000);
