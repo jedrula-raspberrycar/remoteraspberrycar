889c73f22195c20b3693d5b8711eb5796647a944
jedrula <andrzej.swaton@gmail.com>
2017-10-23 01:44:16 +0200

    comment out video for now

--- frontend/ember_cli/app/components/live-feed/component.js
+++ frontend/ember_cli/app/components/live-feed/component.js
@@ -2,15 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   didRender() {
-    // setTimeout(() => {
-      const canvas = this.$('canvas').get(0);
-      // const uri = "ws://192.168.1.102:80/video-stream";
-      const uri = "ws://4de93a31a4324b5dfa4e5e7881681c56.resindevice.io/video-stream";
-      const wsavc = new WSAvcPlayer(canvas, "webgl"); // , 1, 35
-      wsavc.connect(uri);
-      // setTimeout(function() {
-      //   wsavc.playStream()
-      // }, 2000)
-    // }, 2000)
+    // const canvas = this.$('canvas').get(0);
+    // const uri = "ws://4de93a31a4324b5dfa4e5e7881681c56.resindevice.io/video-stream";
+    // const wsavc = new WSAvcPlayer(canvas, "webgl"); // , 1, 35
+    // wsavc.connect(uri);
   }
 });
