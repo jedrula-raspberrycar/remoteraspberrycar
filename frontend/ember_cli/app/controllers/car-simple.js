5643bfa64cd8a2a1a591ecd8d6c099d62d538025
jedrula <andrzej.swaton@gmail.com>
2017-10-23 11:42:43 +0200

    impr

--- frontend/ember_cli/app/controllers/car-simple.js
+++ frontend/ember_cli/app/controllers/car-simple.js
@@ -3,9 +3,4 @@ import Ember from 'ember';
 const { Controller } = Ember;
 
 export default Controller.extend({
-  actions: {
-    updateSpeed(wheel, speed) {
-      wheel.saveSpeed(speed);
-    }
-  }
 })
