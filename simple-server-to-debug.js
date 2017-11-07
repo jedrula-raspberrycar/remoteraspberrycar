0d963a066ededef1fe6e118639a73128b681d9ba
jedrula <andrzej.swaton@gmail.com>
2017-11-07 13:45:10 +0100

    debug

--- simple-server-to-debug.js
+++ simple-server-to-debug.js
@@ -2,15 +2,16 @@
 const express = require('express');
 const cors = require('cors');
 const app = new express();
+const PWM = require('./gpio/raspi-soft-owm-wrapper/raspi-soft-owm-wrapper').PWM;
 app.use(cors());
 const bodyParser = require('body-parser');
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
-app.patch('/test', function(req, res, next) {
-  console.log(req.body);
-  res.json({
-    wow: 'x'
-  })
-});
+Promise.all([PWM(7), PWM(11)]).then(([pwm7, pwm11]) => {
+  pwm7.write(0);
+  pwm11.write(0);
+  // app.patch('/', function(req, res, next) { console.log(req.body); res.json({ wow: 'x' }) });
+})
+
 
 app.listen(8432);
