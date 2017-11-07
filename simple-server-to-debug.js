c38e023d2a8e87805a892edc8e4a04b2718edf61
jedrula <andrzej.swaton@gmail.com>
2017-11-07 13:53:24 +0100

    debugging

--- simple-server-to-debug.js
+++ simple-server-to-debug.js
@@ -8,9 +8,11 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 Promise.all([PWM(7), PWM(11)]).then(([pwm7, pwm11]) => {
-  pwm7.write(0);
-  pwm11.write(0);
-  // app.patch('/', function(req, res, next) { console.log(req.body); res.json({ wow: 'x' }) });
+  app.patch('*', function(req, res, next) {
+    pwm7.write(0);
+    pwm11.write(0);
+    console.log(req.body); res.json({ wow: 'x' });
+  });
 })
 
 
