91f7dcebc207753954ffa62f909b3c5c55618f1e
jedrula <andrzej.swaton@gmail.com>
2017-11-09 17:33:09 +0100

    applying changes to server-simple

--- frontend/ember_cli/app/components/live-feed/component.js
+++ frontend/ember_cli/app/components/live-feed/component.js
@@ -4,8 +4,8 @@ export default Ember.Component.extend({
   didRender() {
     const canvas = this.$('canvas').get(0);
     const wsavc = new WSAvcPlayer(canvas, "webgl"); // , 1, 35
-    const uri = "ws://jedrula.ddns.net:7002/video-stream";
-    // const uri = "ws://192.168.1.201:80/video-stream";
+    // const uri = "ws://jedrula.ddns.net:7002";
+    const uri = "ws://192.168.1.201:80";
     wsavc.connect(uri);
   }
 });
--- simple-server.js
+++ simple-server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const cors = require('cors');
-const app = express();
-require('express-ws')(app);
+const http = require('http');
+const WebSocket = require('ws');
 const raspividStream = require('raspivid-stream');
 const PWM = require('./gpio/raspi-soft-pwm-wrapper/raspi-soft-pwm-wrapper').PWM;
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
 app.use(cors());
 const bodyParser = require('body-parser');
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
@@ -19,28 +24,25 @@ Promise.all([PWM(7), PWM(11), PWM(13), PWM(15)]).then((pwms) => {
   });
 })
 
-app.ws('/video-stream', (ws, req) => {
-    console.log('Client connected');
+wss.on('connection', function connection(ws, req) {
+  console.log('Client connected');
+  ws.send(JSON.stringify({ action: 'init', width: '960', height: '540' }));
 
-    ws.send(JSON.stringify({
-      action: 'init',
-      width: '960',
-      height: '540'
-    }));
+  const videoStream = raspividStream({ rotation: 180 });
 
-    const videoStream = raspividStream({ rotation: 180 });
+  videoStream.on('data', (data) => {
+    if (ws.readyState === WebSocket.OPEN) {
+      ws.send(data, { binary: true }, (error) => { if (error) console.error(error); });
+    }
+  });
 
-    videoStream.on('data', (data) => {
-        ws.send(data, { binary: true }, (error) => { if (error) console.error(error); });
-    });
-
-    ws.on('close', () => {
-        console.log('Client left');
-        videoStream.removeAllListeners('data');
-    });
+  ws.on('close', () => {
+    console.log('Client left');
+    videoStream.removeAllListeners('data');
+  });
 });
 
 const port = parseInt(process.env.PORT || 80);
-app.listen(port, '0.0.0.0', () => {
+server.listen(port, '0.0.0.0', () => {
   console.log(`listening on ${port}`);
 });
