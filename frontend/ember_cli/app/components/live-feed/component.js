172ff3f4eaae3466715b602116aa6eda5d4a1596
jedrula <andrzej.swaton@gmail.com>
2017-11-09 14:18:37 +0100

    cleanup, camera work in ember app

--- frontend/ember_cli/app/components/live-feed/component.js
+++ frontend/ember_cli/app/components/live-feed/component.js
@@ -2,10 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   didRender() {
-    // const canvas = this.$('canvas').get(0);
-    // http://jedrula.ddns.net:7002/wheels/left
-    // const uri = "ws://4de93a31a4324b5dfa4e5e7881681c56.resindevice.io/video-stream";
-    // const wsavc = new WSAvcPlayer(canvas, "webgl"); // , 1, 35
-    // wsavc.connect(uri);
+    const canvas = this.$('canvas').get(0);
+    const wsavc = new WSAvcPlayer(canvas, "webgl"); // , 1, 35
+    const uri = "ws://jedrula.ddns.net:7002/video-stream";
+    wsavc.connect(uri);
   }
 });
