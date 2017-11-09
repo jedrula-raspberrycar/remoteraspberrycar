9772e09edeeb4b68e98133b49b4995541ac25711
jedrula <andrzej.swaton@gmail.com>
2017-11-09 16:36:48 +0100

    try ws instead

--- frontend/camera-only/index.js
+++ frontend/camera-only/index.js
@@ -1,31 +1,34 @@
 // move from frontend direcory!
 const express = require('express');
-const app = express();
-const expressWs = require('express-ws')(app);
+const http = require('http');
+const WebSocket = require('ws');
 const raspividStream = require('raspivid-stream');
-app.ws('/video-stream', (ws, req) => {
-    console.log('Client connected');
 
-    ws.send(JSON.stringify({
-      action: 'init',
-      width: '960',
-      height: '540'
-    }));
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 
-    ws.on('close', () => {
-        console.log('Client left');
-    });
-});
-
-const videoStreamWss = expressWs.getWss('/a');
-
-const videoStream = raspividStream({ rotation: 180 });
-videoStream.on('data', (data) => {
-  videoStreamWss.clients.forEach((client) => {
-    client.send(data, { binary: true }, (error) => { if (error) console.error(error); });
+wss.on('connection', function connection(ws, req) {
+   console.log('Client connected');
+  ws.send(JSON.stringify({
+    action: 'init',
+    width: '960',
+    height: '540'
+  }));
+  ws.on('close', () => {
+      console.log('Client left');
   });
 });
 
-app.listen(80, '0.0.0.0', () => {
+const videoStream = raspividStream({ rotation: 180 });
+videoStream.on('data', (data) => {
+  wss.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(data, { binary: true }, (error) => { if (error) console.error(error); });
+    }
+  });
+});
+
+server.listen(80, '0.0.0.0', () => {
   console.log(`listening on 80`);
 });
