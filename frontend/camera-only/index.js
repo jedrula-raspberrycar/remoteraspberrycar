451add7bf8f7ab03cbcba7da084729825b153488
jedrula <andrzej.swaton@gmail.com>
2017-11-09 17:13:10 +0100

    refactor - working

--- frontend/camera-only/index.js
+++ frontend/camera-only/index.js
@@ -1,3 +1,4 @@
+// FIXME EPIPE error, probably connected to too many listeners, with wss it does not breake though (as it did with express-ws)
 // move from frontend direcory!
 const express = require('express');
 const http = require('http');
@@ -8,14 +9,8 @@ const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-// let counter = 0;
-// const sockets = {};
-
 wss.on('connection', function connection(ws, req) {
   console.log('Client connected');
-  // counter++;
-  // ws.counter = counter;
-  // sockets[counter] = ws;
   ws.send(JSON.stringify({
     action: 'init',
     width: '960',
@@ -31,21 +26,9 @@ wss.on('connection', function connection(ws, req) {
 
   ws.on('close', () => {
     console.log('Client left');
-    // delete sockets[ws.counter];
     videoStream.removeAllListeners('data');
   });
 });
-
-
-// videoStream.on('data', (data) => {
-//   const socketIds = Object.keys(sockets);
-//   socketIds.forEach(function each(key) {
-//     const client = sockets[key];
-//     if (client.readyState === WebSocket.OPEN) {
-//       client.send(data, { binary: true }, (error) => { if (error) console.error(error); });
-//     }
-//   });
-// });
 
 server.listen(80, '0.0.0.0', () => {
   console.log(`listening on 80`);
