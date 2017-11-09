82882222dc0ee46aed53c5a6a60abb26a9deb0ee
jedrula <andrzej.swaton@gmail.com>
2017-11-09 17:10:22 +0100

    add ready state to 1 impl

--- frontend/camera-only/index.js
+++ frontend/camera-only/index.js
@@ -8,37 +8,44 @@ const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-let counter = 0;
-const sockets = {};
+// let counter = 0;
+// const sockets = {};
 
 wss.on('connection', function connection(ws, req) {
   console.log('Client connected');
-  counter++;
-  ws.counter = counter;
-  sockets[counter] = ws;
+  // counter++;
+  // ws.counter = counter;
+  // sockets[counter] = ws;
   ws.send(JSON.stringify({
     action: 'init',
     width: '960',
     height: '540'
   }));
-  ws.on('close', () => {
-      console.log('Client left');
-      delete sockets[ws.counter];
-  });
-});
 
-const videoStream = raspividStream({ rotation: 180 });
-
-
-videoStream.on('data', (data) => {
-  const socketIds = Object.keys(sockets);
-  socketIds.forEach(function each(key) {
-    const client = sockets[key];
-    if (client.readyState === WebSocket.OPEN) {
-      client.send(data, { binary: true }, (error) => { if (error) console.error(error); });
+  const videoStream = raspividStream({ rotation: 180 });
+  videoStream.on('data', (data) => {
+    if (ws.readyState === WebSocket.OPEN) {
+      ws.send(data, { binary: true }, (error) => { if (error) console.error(error); });
     }
   });
+
+  ws.on('close', () => {
+    console.log('Client left');
+    // delete sockets[ws.counter];
+    videoStream.removeAllListeners('data');
+  });
 });
+
+
+// videoStream.on('data', (data) => {
+//   const socketIds = Object.keys(sockets);
+//   socketIds.forEach(function each(key) {
+//     const client = sockets[key];
+//     if (client.readyState === WebSocket.OPEN) {
+//       client.send(data, { binary: true }, (error) => { if (error) console.error(error); });
+//     }
+//   });
+// });
 
 server.listen(80, '0.0.0.0', () => {
   console.log(`listening on 80`);
